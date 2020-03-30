c7be3a1ca40ca0753c587b132e56ca4df3e4a33d
David <david199619@live.co.uk>
2020-03-30 01:19:27 +0100

    Implemnt version0.1 of details modals for submitting action plans/comments Use of Props and visisbility animation to hid and declutter screen

---
+++ components/Sub-Comps/detailsModal.js
@@ -0,0 +1,106 @@
+import React, { Component, useState } from "react";
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  TextInput
+} from "react-native";
+import { Images,Colors,Typography,Spacing } from '../../Styles'
+
+export default class DetailModal extends React.Component{
+    constructor(props) {
+        super(props); 
+        this.state = {
+            Input: "",
+            Type: "",
+            modalVisible: false
+            } 
+               
+        }    
+    setModalVisible(){
+      this.setState({modalVisible: !modalVisible})
+    }
+
+    render() { 
+      return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={this.state.modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>{this.props.title}</Text>
+            <TextInput name="Answer" style={styles.TextInputStyle} value={this.props.existing} onChangeText={(text) => { this.setState({ Input: text})}} />
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                this.setModalVisible();
+                updateInformation(Type,this.state.Input)
+              }}
+            >
+              <Text style={styles.textStyle}>Submit Changes</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+      </View>
+  );
+};
+
+styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  TextInputStyle: {
+    borderColor: Colors.LightGrey.color,
+    borderWidth: 3,
+    padding: 10,
+    color: Colors.Black.color,
+    maxHeight: 50,
+    width: '75%',
+    backgroundColor: Colors.White.color,
+    
+},
+})
+}
