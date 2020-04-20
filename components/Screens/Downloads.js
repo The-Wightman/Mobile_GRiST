a0bc67554ca5504d8857d4ed763ffc6fb5b14588
David <david199619@live.co.uk>
2020-04-20 01:22:10 +0100

    downloads page now has generated content First implementation of fetch Move to use of pdfviewer to allow for offline access

--- components/Screens/Downloads.js
+++ components/Screens/Downloads.js
@@ -15,20 +15,36 @@ import { color } from 'react-native-reanimated';
 import DefaultTemplate from '../Sub-Comps/DefaultScreen'
 import MainHeadTemplate from '../Sub-Comps/Navigation/Header'
 import {Colors,Spacing} from '../../Styles/index'
+import {FunctionCard,cardTypes} from '../Sub-Comps/FunctionCard';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default class Downloads extends Component{ 
   constructor(props) {
     super(props);
     
   }
+  DownloadCards(){
+    let selectedCards = new Array();
+    let index = 0;
+    let foundCards = 0;
+    for(index = 0; index < cardTypes.length; index++){
+      if(cardTypes[index].type == "download"){
+        selectedCards[foundCards] =  <View><FunctionCard {...cardTypes[index]} /></View>  
+        foundCards++        
+      }
+   }
+   selectedCards = <View>{selectedCards}</View>  
+    return selectedCards
+  }
   render() {   
         return(
           <View >
           <MainHeadTemplate navigation={this.props.navigation}/>
-          <DefaultTemplate/>
-          
+          <DefaultTemplate/>          
           <View style={styles.screenPos}>
-          <Text style={styles.TextStyle}>MYProfile</Text>
+          <KeyboardAwareScrollView>
+            {this.DownloadCards()}
+          </KeyboardAwareScrollView>
           </View>
           </View>
 
@@ -48,8 +64,9 @@ const styles = StyleSheet.create({
   },
   screenPos: {
     paddingTop: '15%',
+    paddingBottom: '5%',
     width: '100%',
-    height: '70%',
+    height: '100%',
     alignContent: 'center',
     justifyContent: 'center',
     position: 'absolute',
