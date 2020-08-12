aafe324e8700682713fae0ae5f97899cb463e81c
David <david199619@live.co.uk>
2020-08-12 15:02:37 +0100

    Handler methods for getting the assesment master array for a given user.

--- components/Sub-Comps/userOutline.js
+++ components/Sub-Comps/userOutline.js
@@ -1,12 +1,12 @@
 //Function: localised storage for application information
-//Description: Store and recall information about the user such as the
+//Description: Store and recall information about the user such as the role,assessment information, and connection strings.
 //Inputs: None
 //Outputs: Async functions _storeClient,_storeRole,clearclient, getclient, getrole
 
 //call the Android/IOS agnostic helper library from te react native libraries
 import {AsyncStorage} from 'react-native';
 import {   
-    Alert
+    alert
 } from 'react-native'
 
 //function to handle the server response string,and store it locally for reference when validating internal app functions
@@ -32,6 +32,31 @@ export async function _storeClient(responseJson){
     } 
 
   }
+  //function to store the users Assessment Array
+  export async function _storeAssessArray(UserUID,AssessArray){    
+    try {      
+      //wait for the Storage library to return from storing the key value pair succesfully
+      await AsyncStorage.setItem(UserUID + 'Assessments',JSON.stringify(AssessArray));
+      //catch issues and present them as an error to the user
+    } catch (error) {
+      alert(error)
+    } 
+
+  }
+  // function to return the current clients Assesment array
+  export async function _getAssessArray(UID){    
+    try {
+     //wait for the Storage library to return from retrieving the key value pair succesfully
+     var value = await AsyncStorage.getItem(UID + 'Assessments');
+     //parse the JSON string and then retun the output to the calling component.       
+     return JSON.parse(value)  
+     //catch issues and present them as an error to the user
+    } catch (error) {
+       alert(error)
+    }
+    
+  };
+  
   //function to clear currently stored user session information
   export async function _clearClient(){    
     try {  
