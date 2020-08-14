f657288e9d82292becfb6b47525c2c9608d4cfb2
David <david199619@live.co.uk>
2020-08-14 12:55:02 +0100

    Jest has aerror with running Watchman.clear report on github library for further information. Complete set of V1 snapshot testes for android written.

---
+++ __tests__/ScreenTest.js
@@ -0,0 +1,83 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import {Login,LandingScreen,OverlayScreen,signup,QuestionWindow,MyProfile,MyPatients,MyGroups,MyPlan,MyAssessment,Search,MyReview,Help,Downloads,AssessmentHome} from '../components/Screens';
+
+it('renders correctly', () => {
+  const help = renderer.create(<Help />);
+  expect(help).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const landing = renderer.create(<LandingScreen />);
+  expect(landing).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const login = renderer.create(<Login/>);
+  expect(login).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+    const login = renderer.create(<OverlayScreen/>);
+    expect(login).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const login = renderer.create(<Signup/>);
+    expect(login).toMatchSnapshot();
+  });
+  
+it('renders correctly', () => {
+    const login = renderer.create(<QuestionWindow/>);
+    expect(login).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const login = renderer.create(<MyProfile/>);
+    expect(login).toMatchSnapshot();
+  });
+  it('renders correctly', () => {
+    const login = renderer.create(<MyPatients/>);
+    expect(login).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const login = renderer.create(<MyGroups/>);
+    expect(login).toMatchSnapshot();
+  });
+  
+it('renders correctly', () => {
+    const login = renderer.create(<MyPlan/>);
+    expect(login).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const login = renderer.create(<MyAssessment/>);
+    expect(login).toMatchSnapshot();
+  });
+  it('renders correctly', () => {
+    const login = renderer.create(<Search/>);
+    expect(login).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const login = renderer.create(<MyReview/>);
+    expect(login).toMatchSnapshot();
+  });
+  
+it('renders correctly', () => {
+    const login = renderer.create(<Downloads/>);
+    expect(login).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const login = renderer.create(<AssessmentHome/>);
+    expect(login).toMatchSnapshot();
+  });
+
